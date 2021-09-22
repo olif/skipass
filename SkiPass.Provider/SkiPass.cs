8c9471aabc3620094e616902f0621493dfc3decc
Claes Barthelson <claes.barthelson@gmail.com>
2021-09-22 11:36:17 +0200

    Add project files.

---
+++ SkiPass.Provider/SkiPass.cs
@@ -0,0 +1,12 @@
+﻿namespace SkiPass.Provider
+{
+    public class SkiPass
+    {
+        public Guid ID { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
